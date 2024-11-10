ef772965c6c238221fd7b84be69db42ca3158ad8
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-11-10 23:16:48 +0900

    [10강] 로그인 성공 시 JWT 발급 로직 작성

--- src/main/java/com/example/springhw4/Config/SecurityConfig.java
+++ src/main/java/com/example/springhw4/Config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springhw4.Config;
 
+import com.example.springhw4.jwt.JWTUtil;
 import com.example.springhw4.jwt.LoginFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +22,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
 
     //AuthenticationManager Bean 등록
     @Bean
@@ -61,7 +63,7 @@ public class SecurityConfig {
 
         http
                 // 정확한 자리에 대체 하기 위해 At
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration),jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         // 세션 설정
         http
--- src/main/java/com/example/springhw4/jwt/LoginFilter.java
+++ src/main/java/com/example/springhw4/jwt/LoginFilter.java
@@ -1,5 +1,6 @@
 package com.example.springhw4.jwt;
 
+import com.example.springhw4.dto.CustomUserDetails;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,13 +9,18 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @RequiredArgsConstructor
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     // 검증 담당하는 곳
     private final AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -32,7 +38,22 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     //로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됨)
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
-        System.out.println("배고ㅠ");
+
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        String username = customUserDetails.getUsername();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+
+        String role = auth.getAuthority();
+
+        // jwt 생성
+        String token = jwtUtil.createJwt(username, role, 60*60*10L);
+
+        // response 에 담아서 전달 접두사 Bearer 붙여야 함
+        response.addHeader("Authorization", "Bearer " + token);
     }
 
     //로그인 실패시 실행하는 메소드

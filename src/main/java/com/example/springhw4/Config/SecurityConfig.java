134afdaf93386aa854e32b0c20c82eb75ddd164a
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-11-10 09:25:00 +0900

    [7강] LoginFilter 작성

--- src/main/java/com/example/springhw4/Config/SecurityConfig.java
+++ src/main/java/com/example/springhw4/Config/SecurityConfig.java
@@ -1,17 +1,34 @@
 package com.example.springhw4.Config;
 
+import com.example.springhw4.jwt.LoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -26,21 +43,25 @@ public class SecurityConfig {
         // 세션 방식 : 세션 고정 -> csrf 공격 방어
         // 토큰 방식 : 세션 stateless -> 방어 굳이 x
         http
-                .csrf((auth)-> auth.disable());
+                .csrf(AbstractHttpConfigurer::disable);
 
         // Form 로그인 방식 disable
         http
-                .formLogin((auth) -> auth.disable());
+                .formLogin(AbstractHttpConfigurer::disable);
 
         // http basic 인증 방식 disable
         http
-                .httpBasic((auth) -> auth.disable());
+                .httpBasic(AbstractHttpConfigurer::disable);
 
         http
                 .authorizeHttpRequests((auth)->auth
                         .requestMatchers("login","/","/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
+
+        http
+                // 정확한 자리에 대체 하기 위해 At
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         // 세션 설정
         http
